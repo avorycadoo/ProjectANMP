864dc783a555ad893f05d4d13034f8aca28621d0
avorycadoo <valerinerin24@gmail.com>
2024-10-14 14:01:28 +0700

    Button (blm sls)
    Notes : Pinjem github Valerin
    -Jeanne-

--- app/src/main/java/com/ievana/capygo_anmp/view/GameListAdapter.kt
+++ app/src/main/java/com/ievana/capygo_anmp/view/GameListAdapter.kt
@@ -2,6 +2,7 @@ package com.ievana.capygo_anmp.view
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.navigation.Navigation
 import androidx.recyclerview.widget.RecyclerView
 import com.ievana.capygo_anmp.R
 import com.ievana.capygo_anmp.databinding.CapygoListItemBinding
@@ -38,9 +39,15 @@ class GameListAdapter(val gameList: ArrayList<Game>) : RecyclerView.Adapter<Game
 //            holder.binding.imageView2.setImageResource(R.drawable.background) // Set gambar default jika tidak ada URL
 //        }
 
-//        holder.binding.btnTeams.setOnClickListener {
-//
-//        }
+        holder.binding.btnTeams.setOnClickListener {
+            val action = MainFragmentDirections.actionTeamsDetail()
+            Navigation.findNavController(it).navigate(action)
+        }
+
+        holder.binding.btnAchivement.setOnClickListener {
+            val action = MainFragmentDirections.actionAchivement()
+            Navigation.findNavController(it).navigate(action)
+        }
     }
 
     fun updateGameList(newGame : ArrayList<Game>){
